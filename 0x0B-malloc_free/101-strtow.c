015534ea7e4781e1a0bfc8d14979aa53e1c0a77c
JuneMutten <junemutten@gmail.com>
2023-04-12 11:35:32 -0700

    Correction of memory allocation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,7 +21,7 @@ int count_word(char *s)
 		if (s[c] == ' ')
 			flag = 0;
 		else
-			if (flag == 0)
+			if (flag == 2)
 			{
 				flag = 1;
 				w++;
@@ -54,7 +54,7 @@ char **strtow(char *str)
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
