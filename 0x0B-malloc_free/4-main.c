e7be5e9ae105f35c10da1130bb8c0b68022006e8
JuneMutten <junemutten@gmail.com>
2023-10-03 09:42:53 -0700

    Solution to task 4

--- 0x0B-malloc_free/4-main.c
+++ 0x0B-malloc_free/4-main.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
 
 /**
  * print_grid - prints a grid of integers
