80ede663c241d6555f4a69bfc6c3f96ba0c9015c
JuneMutten <junemutten@gmail.com>
2023-10-05 12:50:59 -0700

    Solution to task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -37,8 +37,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new == NULL)
 		return (NULL);
 
+	copy = ptr
+
 	for (i = 0; i < old_size; i++)
-		new[i] = ptr[i];
+		new[i] = copy[i];
 
 	free(ptr);
 
