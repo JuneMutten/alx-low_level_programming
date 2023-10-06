c86011f9c0e8b4fa182865429877dc461cec9747
JuneMutten <125442147+JuneMutten@users.noreply.github.com>
2023-10-06 08:03:36 +0300

    Editing task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new, *copy;
+	void *new, *copy;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -37,10 +37,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new == NULL)
 		return (NULL);
 
-	copy = ptr;
-
 	for (i = 0; i < old_size; i++)
-		new[i] = copy[i];
+		new[i] = ptr[i];
 
 	free(ptr);
 
