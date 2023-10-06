118c42f86a89f3eb1e0ca035bb2eaeae8b79ed05
JuneMutten <junemutten@gmail.com>
2023-10-05 22:26:49 -0700

    Solution to task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new, *copy;
+	char *new, *copy;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -37,8 +37,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new == NULL)
 		return (NULL);
 
+	copy = ptr;
+
 	for (i = 0; i < old_size; i++)
-		new[i] = ptr[i];
+		new[i] = copy[i];
 
 	free(ptr);
 
