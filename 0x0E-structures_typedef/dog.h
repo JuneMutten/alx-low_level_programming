f141a1aebac14e7816c077163fbfe159b1234f44
JuneMutten <junemutten@gmail.com>
2023-10-09 06:05:20 -0700

    Correcting task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - New type structure 
+ * struct dog - New type structure
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
@@ -18,8 +18,8 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
