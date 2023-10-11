ee17c2a8c807a0f02b4f649314708d101f1f5044
JuneMutten <junemutten@gmail.com>
2023-10-11 04:22:41 -0700

    Correcting if statement

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	if (size < 1)
+	if (array == NULL || cmp == NULL || size < 1)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] != 0))
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 	return (-1);
