7d5cd4128b01edfaead0701a48f02a78eb7486bf
JuneMutten <junemutten@gmail.com>
2023-10-12 12:04:39 -0700

    Correcting task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,7 +25,7 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%c", va_arg(args, int));
+					printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
 				printf("%s%d", separator, va_arg(args, int));
