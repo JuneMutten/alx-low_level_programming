8ca010528c7e2f08fb68ebd4288233faa5daf7d0
JuneMutten <junemutte@gmail.com>
2023-10-26 13:25:40 -0700

    Correcting task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i] != '\0')
 		i++;
 
-	for (i -= 1; i > 0; i--)
+	for (i -= 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
